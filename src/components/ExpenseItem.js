0a2a444ae1d662642fe2d454cd43cf8ea73fe966
Rabee Mghrabi <rabeemghrabi93@gmail.com>
2021-12-06 14:14:26 +0000

    created ExpenseItem component

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function ExpenseItem(){
+  return(
+    <div>
+    <h2> Expense Item </h2>
+    <p> March 8th 2021</p>
+    <h2>Car insurance</h2>
+    <p>$294.67</p>
+    </div>
+
+  )
+}
+
+export default ExpenseItem;
