78c2c470eff7337efc9bb7d3028f3b2c55fbccfd
Rabee Mghrabi <rabeemghrabi93@gmail.com>
2021-12-07 16:26:41 +0000

    splitted the date component and added CSS

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,11 +1,16 @@
 import React from "react";
 import"./ExpenseItem.css"
+import ExpenseDate from "./ExpenseDate.js"
 
 function ExpenseItem(props){
 
+  const month =  props.date.toLocaleString('en-US',{month: 'long'});
+  const day = props.date.toLocaleString('en-US',{day: '2-digit'});
+  const year = props.date.getFullYear();
+
   return(
     <div className="expense-item">
-    <div>{props.date.toISOString()}</div>
+    <ExpenseDate month={month} day={day} year={year}/>
     <div className="expense-item__description">
     <h2>{props.title}</h2>
     <div className="expense-item__price">${props.amount}</div>
