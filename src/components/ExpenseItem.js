d68b1e5a37d386915a80a1affd872503a051b2b0
Rabee Mghrabi <rabeemghrabi93@gmail.com>
2021-12-10 18:00:55 +0000

    refactored CSS and added it to a new cards component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,6 +1,7 @@
 import React from "react";
 import"./ExpenseItem.css"
 import ExpenseDate from "./ExpenseDate.js"
+import Cards from "./Cards.js"
 
 function ExpenseItem(props){
 
@@ -9,13 +10,13 @@ function ExpenseItem(props){
   const year = props.date.getFullYear();
 
   return(
-    <div className="expense-item">
+    <Cards className="expense-item">
     <ExpenseDate month={month} day={day} year={year}/>
     <div className="expense-item__description">
     <h2>{props.title}</h2>
     <div className="expense-item__price">${props.amount}</div>
     </div>
-    </div>
+    </Cards>
 
   )
 }
